f5d2028145959a895aadb625a55d572409264430
svtgo <70940564+svtgo@users.noreply.github.com>
2021-04-02 21:28:05 +0300

    Delete test_hackernews.py

--- homework06/test/test_hackernews.py
+++
@@ -1,7 +0,0 @@
-from hw06 import hackernews
-
-
-def test_clear() -> None:
-    assert hackernews.clean("SVETLANA") == "SVETLANA"
-    assert hackernews.clean("S, V, E, T, A") == "S V E T A"
-    assert hackernews.clean("SVET.lana()") == "SVETlana"
